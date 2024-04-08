e1c6276c24070a11bdc5c513f0ed1e22e5a0424b
pauleramdf <paulomdf741@gmail.com>
2024-04-08 18:03:07 -0300

    Commit inicial

---
+++ src/test/java/com/br/lanchonete/lanchoneteapi/LanchoneteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.lanchonete.lanchoneteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchoneteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
